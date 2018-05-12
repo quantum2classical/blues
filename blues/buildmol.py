fbd779fadd49b13093150682cfea819ac2c87171
Nathan Laurie <natelaur@uw.edu>
2018-05-12 13:38:18 -0700

    transferred to .py from notebook and currently writing test

--- blues/buildmol.py
+++ blues/buildmol.py
@@ -4,35 +4,55 @@ import numpy as np
 from rdkit import Chem
 from rdkit.Chem import AllChem, Draw
 
-def order_atoms(filename='atom_list.txt')
-    """read out the txt file containing all atoms in the molecule, order the same as JANPA output and eliminate whitespace"""
+def order_atoms(filename='atom_list.txt'):
+    """list out the txt file containing all atoms in the molecule, order the same as JANPA output and eliminate whitespace"""
     with open(filename) as f:
         atoms = f.readlines()
         atoms = [atom.strip() for atom in atoms]
+    return atoms
 
-mol = Chem.RWMol()
 
-def
-for atom in atoms:
-    mol.AddAtom(Chem.Atom(atom))
+def add_atoms(atoms):
+    """add atoms and correct bonds to build molecule"""
+    # create molecule under RWMol class, easily editable
+    mol = Chem.RWMol()
+    order_atoms()
+    # add all atoms to molecule
+    for atom in atoms:
+        mol.AddAtom(Chem.Atom(atom))
 
-for i, j in zip(*np.triu_indices_from(bonds, 1)):
-    if np.isclose(bonds[i, j], 1):
-        mol.AddBond(int(i), int(j), Chem.BondType.SINGLE)
-    elif np.isclose(bonds[i, j], 1.5):
-        mol.AddBond(int(i), int(j), Chem.BondType.AROMATIC)
-    elif np.isclose(bonds[i, j], 2):
-        mol.AddBond(int(i), int(j), Chem.BondType.DOUBLE)
-    elif np.isclose(bonds[i, j], 3):
-        mol.AddBond(int(i), int(j), Chem.BondType.TRIPLE)
-    else:
-        pass
 
-for at in mol.GetAtoms():
-    at.SetNoImplicit(True)
+def build_molecule(bonding_matrix):
+    """Takes connection matrix and builds the molecule with RDKit"""
+    # add bonds from connection matrix built from bondorders.py
+    bond = bonding_matrix
+    for i, j in zip(*np.triu_indices_from(bond, 1)):
+        if np.isclose(bonds[i, j], 1):
+            mol.AddBond(int(i), int(j), Chem.BondType.SINGLE)
+        elif np.isclose(bonds[i, j], 1.5):
+            mol.AddBond(int(i), int(j), Chem.BondType.AROMATIC)
+        elif np.isclose(bonds[i, j], 2):
+            mol.AddBond(int(i), int(j), Chem.BondType.DOUBLE)
+        elif np.isclose(bonds[i, j], 3):
+            mol.AddBond(int(i), int(j), Chem.BondType.TRIPLE)
+        else:
+            raise Exception('indeterminate or nonphysical bond type!')
+    # Do not show hydrogens
+    for at in mol.GetAtoms():
+        at.SetNoImplicit(True)
+    return mol
 
-Chem.SanitizeMol(mol)
 
-# 2d representation of molecule
-mol_noH = Chem.RemoveHs(mol)
-Draw.MolToImage(mol_noH)
+def draw_molecule(mol):
+    """Represent built molecule as 2 and 3 dimensional images. Output SMILES and INCHI as well."""
+    # use RDKit internal check to verify molecule is physical
+    Chem.SanitizeMol(mol)
+    mol_noH = Chem.RemoveHs(mol)
+    # 2d representation of molecule
+    two_d = Draw.MolToImage(mol_noH)
+    # 3d picture
+    three_d = Image('mol.png', width=400)
+    inchi = "InChI: {}".format(Chem.MolToInchi(mol_noH))
+    smiles = "SMILES: {}".format(Chem.MolToSmiles(mol_noH))
+
+    return two_d, three_d, inchi, smiles
--- blues/tests/test_buildmol.py
+++ blues/tests/test_buildmol.py
@@ -1,3 +1,12 @@
 from ..buildmol import *
 
+def test_order_atoms():
+    """   """
+    order_atoms(filename='test_atom_list.txt')
+    assert sum(line.isspace() for line in atoms) == 0, 'order_atoms function failed to eliminate whitespace'
+    actual_length = len(atoms)
+    assert len(atoms) == 47, 'molecule should have 47 atoms,' + str(actual_length) + 'was counted'
 
+
+def test_add_atoms():
+    add_atoms()
