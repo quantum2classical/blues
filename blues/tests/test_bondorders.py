2405123459ef6247323853e75f5ea583646ae00b
fduwxcg <xcg27@uw.edu>
2018-05-23 13:33:52 -0700

    tested one fuction

--- blues/tests/test_bondorders.py
+++ blues/tests/test_bondorders.py
@@ -1,1 +1,8 @@
 from ..bondorders import *
+
+def test_convert_bond_orders():
+    bond_orders = get_bond_orders('output.janpa')##should be modified
+    bonding_matrix = convert_bond_orders(bond_orders)
+    assert bonding_matrix.shape[0] == bonding_matrix.shape[1], "the bond orders matrix should be square"
+    assert bonding_matrix[2][2] > 0, "the diagonal of the bond orders matrix should be positive"
+    return
