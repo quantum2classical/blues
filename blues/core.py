e27fc4eca8da3835c5565b5ea4f82f2bbf6bd3b8
Luke Gibson <ldgibson@uw.edu>
2018-05-30 12:44:15 -0700

    added molden2smiles function and a unit test for it

--- blues/core.py
+++ blues/core.py
@@ -1,3 +1,27 @@
-from .bondorders import *
-from .buildmol import *
-from .runjanpa import *
+from rdkit import Chem
+
+from .bondorders import convert_bond_orders
+from .buildmol import build_molecule
+from .runjanpa import run_janpa
+
+
+def molden2smiles(molden_file):
+    """
+    Uses molden file as input to JANPA and generates a smiles string.
+
+    Parameters
+    ----------
+    molden_file : str
+
+    Returns
+    -------
+    smiles : str
+    """
+
+    atoms, raw_bonds = run_janpa(molden_file)
+    bonds = convert_bond_orders(raw_bonds)
+    mol = build_molecule(atoms, bonds)
+    smiles = Chem.MolToSmiles(mol)
+    # two_d, three_d, inchi, smiles = draw_molecule(atoms, bonds)
+
+    return smiles
--- blues/tests/test_core.py
+++ blues/tests/test_core.py
@@ -1,1 +1,14 @@
-from ..core import *
+from ..core import molden2smiles
+from ..utils import file_path
+
+
+def test_molden2smiles():
+
+    real_smiles = 'C=CCc1cccc(C#CC)c1'
+    test_molden = file_path('test.molden', 'tests/test_files')
+
+    test_smiles = molden2smiles(test_molden)
+
+    assert test_smiles == real_smiles
+    
+    return
