d11fd4a8ff580072a7adfe89ff1d68da367b9c29
Luke Gibson <ldgibson@uw.edu>
2018-06-06 14:11:28 -0700

    added functions for core and test functions, also cleaned up flake8 errors

--- blues/buildmol.py
+++ blues/buildmol.py
@@ -1,8 +1,8 @@
-import os
 from IPython.display import Image
 import numpy as np
 from rdkit import Chem
 from rdkit.Chem import Draw
+
 
 def add_atoms(atom_file):
     """
@@ -23,60 +23,75 @@ def add_atoms(atom_file):
     return mol
 
 
-
 def build_molecule(atom_list, bo_matrix):
     """
-    Takes .txt file, builds molecule and connection matrix specifying bonds, and builds the molecule with RDKit
+    Takes .txt file, builds molecule and connection matrix specifying
+    bonds, and builds the molecule with RDKit
 
     Parameters
     ----------
-    atom_file: string of file path
+    atom_file: list of str
+        Atomic symbols of molecule
+    bo_matrix: np.ndarray
+        Array containing bond orders between atoms in the style of a
+        contact matrix.
 
-    Returns:
-
-    RWMol object with structure and bonding based on JANPA's Wiberg-Mayer bond index matrix
-
+    Returns
+    -------
+    RWMol
+        object with structure and bonding based on JANPA's
+        Wiberg-Mayer bond index matrix
     """
+    # Creates structure of molecule.
+    mol = add_atoms(atom_list)
+    for at in mol.GetAtoms():
+        at.SetNoImplicit(True)
+
     # ensure matrix is square
     assert bo_matrix.shape[0] == bo_matrix.shape[1], 'matrix must be square!'
     # ensure diagonals don't equal 0
     for i in range(bo_matrix.shape[0]):
-        assert bo_matrix[i,i] != 0, 'diagonals cannot equal 0'
+        assert np.isclose(bo_matrix[i, i], 0), 'diagonals cannot equal 0'
     for i, j in zip(*np.triu_indices_from(bo_matrix, 1)):
-        if np.isclose(bonds[i, j], 1):
+        if np.isclose(bo_matrix[i, j], 1):
             mol.AddBond(int(i), int(j), Chem.BondType.SINGLE)
-        elif np.isclose(bonds[i, j], 1.5):
+        elif np.isclose(bo_matrix[i, j], 1.5):
             mol.AddBond(int(i), int(j), Chem.BondType.AROMATIC)
-        elif np.isclose(bonds[i, j], 2):
+        elif np.isclose(bo_matrix[i, j], 2):
             mol.AddBond(int(i), int(j), Chem.BondType.DOUBLE)
-        elif np.isclose(bonds[i, j], 3):
+        elif np.isclose(bo_matrix[i, j], 3):
             mol.AddBond(int(i), int(j), Chem.BondType.TRIPLE)
-        elif np.isclose(bonds[i, j], 0):
+        elif np.isclose(bo_matrix[i, j], 0):
             pass
         else:
             raise Exception('indeterminate or nonphysical bond type!')
+
     # Do not show hydrogens
-    mol = add_atoms(atoms)
-    for at in mol.GetAtoms():
-        at.SetNoImplicit(True)
+    mol = Chem.RemoveHs(mol)
     return mol
 
 
-def draw_molecule(atom_list):
+def draw_molecule(atom_list, bo_matrix):
     """
-    Represents built molecule as 2 and 3 dimensional images and output INCHI and SMILES. Molecule is built from
-    txt file containing ordered elements, bonds are added from Wiberg-Mayer matrix contained in JANPA output
+    Represents built molecule as 2 and 3 dimensional images and output
+    INCHI and SMILES. Molecule is built from txt file containing ordered
+    elements, bonds are added from Wiberg-Mayer matrix contained in
+    JANPA output.
 
     Parameters
     ----------
-    atom_list: string containing file path
+    atom_file: list of str
+        Atomic symbols of molecule
+    bo_matrix: np.ndarray
+        Array containing bond orders between atoms in the style of a
+        contact matrix.
 
     Returns:
     ----------
     2d and 3d images as well as INCHI and SMILES strings for the molecule
     """
     # build molecule
-    mol = build_molecule(atom_list)
+    mol = build_molecule(atom_list, bo_matrix)
     # 2d representation of molecule
     two_d = Draw.MolToImage(mol)
     # 3d picture
--- blues/core.py
+++ blues/core.py
@@ -1,7 +1,7 @@
 from rdkit import Chem
 
 from .bondorders import convert_bond_orders
-from .buildmol import build_molecule
+from .buildmol import build_molecule, draw_molecule
 from .runjanpa import run_janpa
 
 
@@ -25,3 +25,40 @@ def molden2smiles(molden_file):
     # two_d, three_d, inchi, smiles = draw_molecule(atoms, bonds)
 
     return smiles
+
+
+def molden2inchi(molden_file):
+    """Uses molden file as input to JANPA and generates an InChI string.
+
+    Parameters
+    ----------
+    molden_file : str
+
+    Returns
+    -------
+    inchi : str
+    """
+    atoms, raw_bonds = run_janpa(molden_file)
+    bonds = convert_bond_orders(raw_bonds)
+    mol = build_molecule(atoms, bonds)
+    inchi = Chem.MolToInchi(mol)
+
+    return inchi
+
+
+def molden2lewis(molden_file, filename=None, save=False):
+    """Uses molden file as input to JANPA and generates a 2D Lewis structure.
+
+    Parameters
+    ----------
+    molden_file : str
+
+    Returns
+    -------
+    lewis : image
+    """
+    atoms, raw_bonds = run_janpa(molden_file)
+    bonds = convert_bond_orders(raw_bonds)
+    lewis, three_d, inchi, smiles = draw_molecule(atoms, bonds)
+
+    return lewis
