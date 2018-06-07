7d7b4f4dab0cde00894a902ecc2cd0bc80ea02a7
Luke Gibson <ldgibson@uw.edu>
2018-06-07 10:40:09 -0700

    added other core modules

--- blues/__init__.py
+++ blues/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import absolute_import, division, print_function
 from .version import __version__  # noqa
-from .core import molden2smiles  # noqa
+from .core import molden2smiles, molden2inchi, molden2lewis  # noqa
